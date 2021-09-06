234aad8c7cadf1a9fe9c4eac66fdaf9b88a29f03
Shamim Hossain <hossains159@gmail.com>
2021-09-06 06:34:48 +0600

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useHead } from '@vueuse/head'
+
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'Medium Like Blog (Vue + Vite)',
+  meta: [
+    { name: 'description', content: 'A Medium like blog using Vue 3 + Vite + Typescript etc.' },
+  ],
+})
+</script>
+
+<template>
+  <router-view />
+</template>
